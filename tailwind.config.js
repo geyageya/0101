bc14c8a57b94e56077a54d685db3de6854c4228f
geyageya <kageya@kaigai21.com>
2022-01-08 11:42:30 +0900

    🔧 tailwind config更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
 
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: [],
+  }
+  // plugins: [],
 }
