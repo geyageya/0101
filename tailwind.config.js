2b7efa269b377b8de6f7f87b80d8629aedc133da
geyageya <kageya@kaigai21.com>
2022-01-12 11:32:04 +0900

    🩹 💄

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,9 @@ module.exports = {
   ],
   theme: {
     
-    extend: {
-      gridTemplateColumns: {
-      // Simple 16 column grid
-      '3x100': 'repeat(3, minmax(100px, 1fr))',
-      '6x150': 'repeat(6, minmax(150px, 1fr))',
-
-      // Complex site-specific column configuration
-      'footer': '200px minmax(900px, 1fr) 100px',
-      }
-    }
+    extend: []
+    
+    
   }
   // plugins: [],
 }
