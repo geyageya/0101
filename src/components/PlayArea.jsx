68cc22e63dd12ed3c41b12130747c278fdf594ce
geyageya <kageya@kaigai21.com>
2022-01-18 15:18:18 +0900

    ♻️

--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -1,20 +1,55 @@
-import {memo} from "react";
+// import {memo} from "react";
 import { MiniArea } from "./MiniArea";
 import { ListCards } from "./ListCards";
+import { useSetup } from "../hooks/useSetup";
 
 export const PlayArea =(props) => {
     console.log("PlayArea - Child1");
+
+
+    const {area} =useSetup
+
+    let backgroundImage="";
+
+{switch(area){
+  case "Asia":
+    backgroundImage="url(../images/worldmap.png)";
+    break
+  case "Europe":
+    backgroundImage="url(../images/tatami-1.png)" ;
+  break
+  case "Africa":
+    backgroundImage="url(../images/tatami-1.png)" ;
+  break
+  case "Americas":
+    backgroundImage="url(../images/tatami-1.png)" ;
+  break
+  case "Oceania":
+    backgroundImage="url(../images/tatami-1.png)" ;
+  break
+
+  case "World":
+    backgroundImage="url(../images/worldmap2.png)"; 
+  break
+
+  default: //Worldに同じ
+    backgroundImage="url(../images/tatami-1.png)";
+
+  }
+}//switch
     const playArea = {
    
       //差し込み用(props)
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
-      // backgroundImage: props.backgroundImage, 
+      backgroundImage: props.backgroundImage, 
     }
     return(
       <main 
-        className ={`h-[454px] md:h-[630px] lg:h-[520px] bg-sky-100 mx-auto relative flex justify-center bg-cover bg-no-repeat ${props.backgroundImage} `}
-        style={playArea}>
+        // className ="h-[454px] md:h-[630px] lg:h-[520px] bg-sky-100 mx-auto relative flex justify-center bg-contain bg-no-repeat bg-[url('https://karuta2020.tokyo/wp-content/uploads/gallery/2106krt_001_c.jpg')]"
+        className ={`h-[454px] md:h-[630px] lg:h-[520px] bg-sky-100 mx-auto relative flex justify-center bg-cover bg-no-repeat ${backgroundImage} `}
+        style={playArea}
+        >
         <ListCards
           //const CardGrid用
           karutaLists={props.karutaLists}
@@ -39,12 +74,16 @@ export const PlayArea =(props) => {
         <MiniArea
           miniArray = {props.miniCard} 
           tailwind="bottom-0 left-0 items-end"
+          // isScored={isScored}
+          currentTurn={props.currentTurn}
          
         />
         {/* PC用 */}
         <MiniArea
           miniArray = {props.miniCardPc} 
           tailwind="top-0 left-0 items-start"
+          // isScored = {isScored}
+          currentTurn={props.currentTurn}
         />
         {/* items-start, items-end 札を上下線にそって配列。外側が直線、内側が凸凹*/}
         </main>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ export const Title=memo((props)=>{
     
     return(
       <h1 
-        className ="p-1 bg-red-200 text-2xl text-center md:w-[768px] md:p-3  md:bg-amber-300 lg:container lg:w-[1024px] lg:bg-lime-300 lg:text-3xl xl:container xl:w-[1280px] xl:bg-sky-500"
+        className ="p-1 mx-auto bg-red-200 text-2xl text-center md:w-[768px] md:p-3  md:bg-amber-300 lg:container lg:w-[1024px] lg:bg-lime-300 lg:text-3xl xl:container xl:w-[1280px] xl:bg-sky-500"
       >
          世界200ヶ国かるた!
         {/* {props.children} */}
