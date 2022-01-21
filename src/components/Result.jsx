fc00c8a8c1963587e7ad0612f170c61bc01d0ec0
geyageya <kageya@kaigai21.com>
2022-01-21 19:02:58 +0900

    ♻️ 関数newGame移動

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,6 +4,11 @@ import { WindowBtn } from "./WindowBtn";
 export const Result = (props)=>{
   console.log("Result - Child2");
 
+  //次のゲーム
+  const newGame =() => {
+    window.location.reload();
+  }
+
     return(
       <div 
         className  = "absolute top-[200px] w-[200px] left-0 right-0 mx-auto  p-5 border-2  rounded-md text-center bg-gray-100 shadow-md"
@@ -43,7 +48,7 @@ export const Result = (props)=>{
          {props.score}枚取りました。
         </p>
           <WindowBtn 
-            onClick={props.onClick} 
+            onClick={()=>newGame()} 
             windowBtnMsg="もう一回？" 
           />
     </div>
