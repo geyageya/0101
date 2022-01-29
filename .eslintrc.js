28132f89d8bf792d3c3ed0e5b975873652bc2b8c
geyageya <kageya@kaigai21.com>
2022-01-29 15:29:49 +0900

    ➕ eslint, prettier設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  //一番効いてほしいものを最後に配置（shimabu video)
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "plugin:react/recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        
+    // "ecmaVersion": "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // "semi": ["error", "always"],
+    // "quotes":["error", "double"],
+    "no-undef": "error",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    //景谷追加
+    "react/display-name": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes":["error", "double"],
-        "react/prop-types":"off",
-        "react/display-name":"off",
-        // "no-undef": "error"
-    }
+  },
 };
