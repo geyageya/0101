bd753f5fc510a5c2bbb02b446028d25406693559
geyageya <kageya@kaigai21.com>
2024-03-31 11:40:14 +0900

    タイトル変更

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,7 +7,7 @@ export const Title = memo(() => {
 
   return (
     <h1 className="p-1 mx-auto bg-red-200 text-3xl text-center md:w-[768px] md:p-3  md:bg-amber-300 lg:container lg:w-[1024px] lg:bg-lime-300 lg:text-3xl xl:container xl:w-[1280px] xl:bg-sky-500">
-      「世界200ヶ国かるた」で、あそぼう! 英語でもあそべるよ
+      「世界200ヶ国かるた」で、あそぼう! 
       {/* {props.children} */}
     </h1>
   );
